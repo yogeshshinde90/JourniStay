7e728e09fd6502d06791fbebf33e52d50ebad0af
yogeshshinde90 <yogeshs9762@gmail.com>
2025-07-04 22:14:01 +0530

    updated for express-router

--- JourniStay/app.js
+++ JourniStay/app.js
@@ -2,62 +2,65 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const methodOverride = require("method-override");
+
+const ExpressError = require("./utils/ExpressError.js");
+
+const listings = require("./routes/listing.js")
+const reviews = require("./routes/review.js")
+
 
 const port = 8080;
-const Listing = require("./models/listings");
-app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended : true}));
 
-
-
-
+// Database connection
 async function main() {
     await mongoose.connect("mongodb://localhost:27017/JourniStay");
 }
-
 main()
-.then(() => {
-    console.log("connected to database");
-})
-.catch(err=> {
-    console.error("error connecting to database",err);
-})
+    .then(() => console.log("✅ Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
-//root route
-app.get("/",(req,res) => {
-    res.send("route is working");
-})
+// Middleware
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-//index route
+// Routes
 
-app.get("/listings" , async (req,res) => {
-  const alllistings = await Listing.find({});
-  res.render("listings/index.ejs",{alllistings});
-    
+// Root
+app.get("/", (req, res) => {
+    res.send("✅ Root Route Working");
 });
 
-//new route
-app.get("/listings/new",  (req,res) => {
-    res.render("listings/new.ejs")
-});
-
-//show route
-app.get("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-  const listing =  await  Listing.findById(id);
-  res.render("listings/show.ejs",{listing});
-});
+app.use("/listings", listings)
+app.use("/listings/:id/reviews", reviews)
 
 
-//create route
-app.post("/listings",async (req,res) => {
-   const newlisting = new Listing(req.body.listing);
-   await newlisting.save();
-   res.redirect("/listings")
+
+// Index
+
+
+// 404 Route
+app.all("{/*any}", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
 });
 
 
-app.listen(port,() => {
-    console.log("server is running");
-})
+
+// Error Handler
+app.use((err, req, res, next) => {
+    const { statuscode = 500, message = "Something went wrong" } = err;
+    res.status(statuscode).render("error.ejs",{message});
+});
+
+
+
+// Start server
+app.listen(port, () => {
+    console.log(` Server is running on http://localhost:${port}`);
+});
