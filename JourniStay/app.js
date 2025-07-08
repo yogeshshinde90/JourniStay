c898bfa9c7ba9b837c80bf01e26c8282218f5114
yogeshshinde90 <yogeshs9762@gmail.com>
2025-07-08 23:07:42 +0530

    implement passport

--- JourniStay/app.js
+++ JourniStay/app.js
@@ -5,10 +5,14 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError.js");
-const listings = require("./routes/listing.js")
-const reviews = require("./routes/review.js")
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+const user = require("./routes/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 const port = 8080;
@@ -48,18 +52,36 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/demouser", async (req,res) => {
+    let fakeuser = new User({
+        email:"student@gmail.com",
+        username:"delta-student2"
+    });
+
+   let registerUser = await   User.register(fakeuser,"helloworld");
+   res.send(registerUser);
 })
 
 // Routes
 
-/
+
 
 app.use("/listings", listings)
 app.use("/listings/:id/reviews", reviews)
+app.use("/",user);
 
 
 
